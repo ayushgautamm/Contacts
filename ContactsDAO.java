a009427736de1fb470dc943121ca77beaa6f6917
Ayush Kumar Gautam <157630163+ayushgautamm@users.noreply.github.com>
2024-09-08 17:36:37 +0530

    Create ContactsDAO.java

---
+++ ContactsDAO.java
@@ -0,0 +1,21 @@
+package com.example.contactsapp;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ContactsDAO {
+     @Insert
+    void insert(Contacts contacts);
+     @Delete
+    void delete(Contacts contacts);
+
+     @Query("SELECT * FROM  contacts_table")
+     LiveData<List<Contacts>> getallContacts();
+
+}
